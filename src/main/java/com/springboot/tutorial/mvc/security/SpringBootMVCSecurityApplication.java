a44417783ef8c015b84195bd12399c26c1b50ed7
Reggie <reggie.senal@gmail.com>
2024-09-07 08:44:45 +0800

    springboot mvc security

---
+++ src/main/java/com/springboot/tutorial/mvc/security/SpringBootMVCSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tutorial.mvc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMVCSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMVCSecurityApplication.class, args);
+    }
+
+}
