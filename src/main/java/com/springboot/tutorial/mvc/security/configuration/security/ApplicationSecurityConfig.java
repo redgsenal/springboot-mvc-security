e42c4a84bb896a8b7c098c69a6e576de6212e6ec
Reggie <reggie.senal@gmail.com>
2024-09-08 10:23:45 +0800

    add login page and authetication add bootstrap

--- src/main/java/com/springboot/tutorial/mvc/security/configuration/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springboot/tutorial/mvc/security/configuration/security/ApplicationSecurityConfig.java
@@ -2,12 +2,37 @@ package com.springboot.tutorial.mvc.security.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ApplicationSecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer configureWebSecurity() {
+        return (web) -> web.ignoring().requestMatchers("/scripts/**", "/styles/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                configurer
+                -> configurer
+                        .anyRequest().authenticated()
+        )
+                .formLogin(
+                        form
+                        -> form
+                                .loginPage("/showLoginPage")
+                                .loginProcessingUrl("/authenticateUser")
+                                .permitAll()
+                );
+        return httpSecurity.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
